da942a86223409b360d98061175281a6d61a6f74
itsautfullday <33750588+itsautfullday@users.noreply.github.com>
2019-10-30 00:17:42 +0530

    GroupAddition->Addmembers

---
+++ GroupAddition.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GroupAddition.aspx.cs" Inherits="WebApplication3.GroupAddition" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+            Name of the Group:</p>
+        <p>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Group Details:</p>
+        <p>
+            <asp:TextBox ID="TextBox2" runat="server" Height="101px"  Width="579px"></asp:TextBox>
+        </p>
+        <p>
+            Group Leader:</p>
+        <p>
+            Name :<asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp; Registration Number:<asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="Button2" runat="server" Text="Create Groups" OnClick ="Button2_Click" />
+        </p>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </p>
+        <p>
+            <asp:Button ID="Button1" runat="server" Text="ADD MEMBERS" OnClick="Button1_Click" />
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ GroupAddition.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.Sql;
+
+
+namespace WebApplication3
+{
+    public partial class GroupAddition : System.Web.UI.Page
+    {
+       
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Session["GroupName"] = TextBox1.Text;
+            Response.Redirect("AddMembers.aspx");
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;" + "Initial Catalog=UserDetails;Integrated Security=SSPI"))
+            {
+                using (SqlCommand cmd = new SqlCommand("insert into groups values (@f1,@f2,@f3,@f4)", con))
+                {
+                    con.Open();
+                    cmd.Parameters.AddWithValue("f1", TextBox1.Text);
+                    cmd.Parameters.AddWithValue("f2", TextBox2.Text);
+                    cmd.Parameters.AddWithValue("f4", TextBox3.Text);
+                    int m;
+                    int.TryParse(TextBox4.Text, out m);
+                    cmd.Parameters.AddWithValue("f3",m);
+                    int l = cmd.ExecuteNonQuery();
+                    if (l != 0)
+                    {
+
+                        Label1.Text = "Success";
+                        cmd.CommandText = "CREATE TABLE [dbo]."+ TextBox1.Text +"([lead_reg] INT NOT NULL,[lead_name] TEXT NOT NULL)";
+             
+                        if(cmd.ExecuteNonQuery() != 0)
+                        {
+                            Label1.Text = "Group Created";
+                        }
+
+                    }
+                    else {
+                        Label1.Text = "NOT";
+                    }
+                    
+                }
+            }
+
+        }
+
+       
+    }
+}
